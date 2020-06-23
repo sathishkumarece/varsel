e9835a9b1e49548408561e2e850cdece5eb6bb15
sathishkumar NATARAJ <32127136+sathishkumarece@users.noreply.github.com>
2020-06-23 22:28:43 +0530

    V2.0.2 - Application port update

--- server.js
+++ server.js
@@ -168,10 +168,7 @@ function isRememberMe() {
     }
 }
 
-var port = 1516;
-if (process.env.APPLICATION_SERVICE_PORT) {
-    port = process.env.APPLICATION_SERVICE_PORT;
-}
+var port = process.env.APPLICATION_SERVICE_PORT | process.env.PORT | 1516;
 app.listen(port, () => {
     console.log('Server started!!!');
 
